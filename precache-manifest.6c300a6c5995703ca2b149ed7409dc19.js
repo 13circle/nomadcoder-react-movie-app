1f4c95edca823571a9cdfe2fd9a271cf59071814
13circle <13circle97@gmail.com>
2020-08-15 01:36:18 +0900

    Updates

---
+++ precache-manifest.6c300a6c5995703ca2b149ed7409dc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd28e3d68f80a7a92f74927615c2bb9",
+    "url": "/nomadcoder-react-movie-app/index.html"
+  },
+  {
+    "revision": "5b803de81b6d56e6ef95",
+    "url": "/nomadcoder-react-movie-app/static/css/main.d79977af.chunk.css"
+  },
+  {
+    "revision": "0d49e99ef25e0b493181",
+    "url": "/nomadcoder-react-movie-app/static/js/2.d87e97f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomadcoder-react-movie-app/static/js/2.d87e97f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b803de81b6d56e6ef95",
+    "url": "/nomadcoder-react-movie-app/static/js/main.fb7e5452.chunk.js"
+  },
+  {
+    "revision": "cfb70030404c26e2d799",
+    "url": "/nomadcoder-react-movie-app/static/js/runtime-main.d23b98f2.js"
+  }
+]);
